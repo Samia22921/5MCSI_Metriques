ac515896de034f8e1efbc14a79e93ee7086ad0e7
Samia22921 <amoussasamiia@gmail.com>
2025-02-19 16:33:07 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,10 +1,18 @@
-from flask import Flask, 
+from flask import Flask, render_template_string, render_template, jsonify
+from flask import render_template
+from flask import json
+from datetime import datetime
+from urllib.request import urlopen
+import sqlite3
                                                                                                                                        
 app = Flask(__name__)   
 @app.route("/contact/")
 def MaPremiereAPI():
     return "<h2>Ma page de contact</h2>"
-                                                                                                                                      
+                                                                                                                                       
+@app.route('/')
+def hello_world():
+    return render_template('hello.html') #Comm1
   
 if __name__ == "__main__":
   app.run(debug=True) #Ajout de la route /contact/
