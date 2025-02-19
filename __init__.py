5f2d0400df3805be1109a7d2d1b61db48e9961d8
Samia22921 <amoussasamiia@gmail.com>
2025-02-19 16:48:45 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -9,6 +9,10 @@ app = Flask(__name__)
 @app.route("/contact/")
 def MaPremiereAPI():
     return "<h2>Ma page de contact</h2>"
+                                                                                                                                       
+@app.route('/')
+def hello_world():
+    return render_template('hello.html') #Comm1
   
 if __name__ == "__main__":
   app.run(debug=True) #Ajout de la route /contact/
