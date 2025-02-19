b6140efcfe8515ac84ee5f4dae041053667719d9
Samia22921 <amoussasamiia@gmail.com>
2025-02-19 17:46:35 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -5,11 +5,22 @@ from datetime import datetime
 from urllib.request import urlopen
 import sqlite3
                                                                                                                                        
+                                                                                                                                       
+@app.route('/')
+def hello_world():
+    return render_template('hello.html') #Comm1
+@app.route("/contact/")
+def MaPremiereAPI():
+    return "<h2>Ma page de contact</h2>"
+  
+if __name__ == "__main__":
+  app.run(debug=True) #Ajout de la route /contact/
+
+                                                                                                                                       
 app = Flask(__name__)   
 @app.route("/rapport/")
 def mongraphique():
     return render_template("graphique.html")
                                                                                                                                        
   
-if __name__ == "__main__":
-  app.run(debug=True)
+
