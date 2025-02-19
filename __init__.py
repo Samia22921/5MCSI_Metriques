e11425d9fc202df6b5542cbad076b72f139a70aa
Samia22921 <amoussasamiia@gmail.com>
2025-02-19 16:44:16 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -8,7 +8,8 @@ import sqlite3
 app = Flask(__name__)   
 @app.route("/contact/")
 def MaPremiereAPI():
-    return "<h2>Ma page de contact</h2>"
-
+    return render_template('Ma page de contact') #Ajout de la nouvelle route
+  
 if __name__ == "__main__":
   app.run(debug=True) #Ajout de la route /contact/
+
