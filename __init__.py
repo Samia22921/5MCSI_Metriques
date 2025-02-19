3f5960832eaea331c6dd21aee7f101d14bb6692e
Samia22921 <amoussasamiia@gmail.com>
2025-02-19 18:28:45 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,5 +1,3 @@
-
-Mon code de base :
 from flask import Flask, render_template_string, render_template, jsonify
 from flask import render_template
 from flask import json
@@ -19,17 +17,12 @@ def meteo():
         temp_day_value = list_element.get('main', {}).get('temp') - 273.15 # Conversion de Kelvin en °c 
         results.append({'Jour': dt_value, 'temp': temp_day_value})
     return jsonify(results=results)
-@app.route("/contact/")
-def MaPremiereAPI():
-    return "<h2>Ma page de contact</h2>"
                                                                                                                                        
 @app.route('/')
 def hello_world():
     return render_template('hello.html') #Comm1
   
 if __name__ == "__main__":
-  app.run(debug=True) #Ajout de la route /contact/
-
-                                                                                                                                                                                                                   
+  app.run(debug=True)                                                                                                                                                                           
   
 
