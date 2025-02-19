04d134c60f33856e5130dd64ffa74fd598744156
Samia22921 <amoussasamiia@gmail.com>
2025-02-19 18:52:08 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -7,13 +7,10 @@ import sqlite3
                                                                                                                                        
 app = Flask(__name__)   
 
-@app.route("/rapport/")
+@app.route('/rapport/')
 def mongraphique():
     return render_template("graphique.html")
                                                                                                                                        
-@app.route('/')
-def hello_world():
-    return render_template('hello.html') #Comm1
   
 if __name__ == "__main__":
   app.run(debug=True)    
