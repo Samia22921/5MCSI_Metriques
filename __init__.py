77b3f1a1c189e2403c4c6a5785c740749df1ce8b
Samia22921 <amoussasamiia@gmail.com>
2025-02-19 18:47:38 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -17,24 +17,16 @@ def meteo():
         temp_day_value = list_element.get('main', {}).get('temp') - 273.15 # Conversion de Kelvin en °c 
         results.append({'Jour': dt_value, 'temp': temp_day_value})
     return jsonify(results=results)
+                                                                                                                                       
 
 @app.route("/rapport/")
 def mongraphique():
     return render_template("graphique.html")
-                                                                                                                                       
-@app.route('/')
-def hello_world():
-    return render_template('hello.html') #Comm1
   
 if __name__ == "__main__":
   app.run(debug=True)
 
 
-
-
-@app.route("/rapport/")
-def mongraphique():
-    return render_template("graphique.html")
                                                                                                                                        
 
 
