f23a386fa3adf9f46b511781a08dd3317a07dd1e
Samia22921 <amoussasamiia@gmail.com>
2025-02-21 10:53:18 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,9 +1,5 @@
 from flask import Flask, render_template_string, render_template, jsonify
 from flask import render_template
-from flask import json
-from datetime import datetime
-from urllib.request import urlopen
-import sqlite3
                                                                                                                                        
 app = Flask(__name__)   
  
