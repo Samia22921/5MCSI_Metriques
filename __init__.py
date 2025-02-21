fbcba7cefc4294c65b5945ccd76c06bf4ff04eaf
Samia22921 <amoussasamiia@gmail.com>
2025-02-21 11:05:57 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
   
 @app.route('/')
 def contact():
-    return render_template('contact.html') #Comm1
+    return render_template('contact.html') #Contact
                                                                                                                                        
 if __name__ == "__main__":
   app.run(debug=True)    
